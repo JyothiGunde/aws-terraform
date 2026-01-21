41c50c70a345198c79bfc5cbb7487b259f379ec7
JyothiGunde <jyothigunde468@gamil.com>
2026-01-22 01:12:13 +0530

    destroy fix lambda zip

--- modules/lambda/terraform.tf
+++ modules/lambda/terraform.tf
@@ -8,11 +8,18 @@ resource "aws_lambda_function" "tf_lambda" {
   source_code_hash = filebase64sha256("lambda.zip")
 
   timeout = 30
+
+  lifecycle {
+    ignore_changes = [
+      filename,
+      source_code_hash
+    ]
+  }
 }
 
 resource "aws_lambda_permission" "permission" {
   statement_id = "AllowExecutionFromS3"
-  action = "lambda:Invokefunction"
+  action = "lambda:InvokeFunction"
   function_name = aws_lambda_function.tf_lambda.function_name
   principal = "s3.amazonaws.com"
   source_arn = var.s3_bucket
