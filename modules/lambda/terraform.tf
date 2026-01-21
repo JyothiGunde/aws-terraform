92663c4bd54f41d4d1423616aee5bc6491651505
JyothiGunde <jyothigunde468@gamil.com>
2026-01-22 01:28:42 +0530

    Fix cleanup

--- modules/lambda/terraform.tf
+++ modules/lambda/terraform.tf
@@ -4,17 +4,11 @@ resource "aws_lambda_function" "tf_lambda" {
   handler = "lambda_function.lambda_handler"
   runtime = "python3.12"
 
-  filename         = "${path.module}/lambda.zip"
-  source_code_hash = filebase64sha256("${path.module}/lambda.zip")
+  filename = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
 
   timeout = 30
-
-  lifecycle {
-    ignore_changes = [
-      filename,
-      source_code_hash
-    ]
-  }
+  
 }
 
 resource "aws_lambda_permission" "permission" {
