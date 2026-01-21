2121997a1511237a941d3a5cb090f807545bff3f
JyothiGunde <jyothigunde468@gamil.com>
2026-01-21 23:58:35 +0530

    Fix lambda role

--- modules/iam/terraform.tf
+++ modules/iam/terraform.tf
@@ -81,22 +81,3 @@ resource "aws_iam_role_policy" "tf_lambda_policy" {
     }]
   })
 }
-
-resource "aws_iam_role_policy" "tf_lambda_policy" {
-  name = "tf_lambda_policy"
-  role = aws_iam_role.tf_lambda_role.id 
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-        Effect = "Allow"
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
