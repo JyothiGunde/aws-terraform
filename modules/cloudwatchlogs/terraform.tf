58af928d94923f215b7172eb996d75e644cabbfe
JyothiGunde <jyothigunde468@gamil.com>
2026-01-27 23:40:08 +0530

    Final run

--- modules/cloudwatchlogs/terraform.tf
+++ modules/cloudwatchlogs/terraform.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "lambda" {
-  name              = "/aws/lambda/example-lambda"
+  name              = "/aws/lambda/s3-triggers-lambda"
   retention_in_days = 7
 }
